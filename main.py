b0ec2547d7ddc67faf1aadabeeb194edb4dc8769
WoccPocc <paulalexism@gmail.com>
2026-05-26 00:14:16 -0600

    Add user input
    add user input and a sorta working context

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("GEMINI_API_KEY not found")
+
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+
+    response = client.models.generate_content(
+        model = 'gemini-2.5-flash', 
+        contents = messages
+        )
+    if response.usage_metadata is None:
+        raise RuntimeError("NO METADATA")
+    print(
+        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count} "
+        )
+    print(response.text)
+
 
 
 if __name__ == "__main__":
